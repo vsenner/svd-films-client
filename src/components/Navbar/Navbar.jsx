213329a3624af2eb27d33fc89af17d18d25c8074
Androseiv <kakatinushka@gmail.com>
2021-11-27 13:51:35 +0200

    Created Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import './Navbar.scss'
+import {Link} from "react-router-dom";
+import logo from "../../images/icons8-film-64.png"
 
 const Navbar = () => {
-
   return (
-    <div>
-
+    <div className='navbar'>
+      <div className="container">
+        <div className="navbar__flex">
+          <div className="navbar__logo">
+            <Link to='/'>
+              <img src={logo} alt="logo"/>
+            </Link>
+          </div>
+          <div className="navbar__categories">
+            <Link to='' className="navbar__link">
+              Movies
+            </Link>
+            <Link to='' className="navbar__link">
+              Series
+            </Link>
+            <Link to='' className="navbar__link">
+              Cartoons
+            </Link>
+            <Link to='' className="navbar__link">
+              TV Shows
+            </Link>
+          </div>
+          <div className="navbar__wide-area">
+            {/*TODO: Change to custom input*/}
+            <input className="navbar__searchbar"/>
+          </div>
+          <Link to={'/account'}>
+            <div className='navbar__account'>
+              <img src="" alt=""/>
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
