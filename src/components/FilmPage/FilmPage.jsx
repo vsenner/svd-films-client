d4dd3e658704121d9d16b3ab328ddbe4eb6348a8
Androseiv <kakatinushka@gmail.com>
2021-12-04 15:51:51 +0200

    Created GenreList Component, small fixes with main page, added new hook, UI component "Button"

--- src/components/FilmPage/FilmPage.jsx
+++ src/components/FilmPage/FilmPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './FilmPage.scss'
 
-
-
 const FilmPage = () => {
   return (
     <div>
--- src/components/GenrePage/GenrePage.jsx
+++ src/components/GenrePage/GenrePage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './GenrePage.scss'
 import GenreList from "./GenreList/GenreList";
+import FilmList from "./FilmList/FilmList";
+import {useParams} from "react-router-dom";
 
 const GenrePage = () => {
+
+  const a = useParams();
+  console.log(a)
+
   return (
     <div>
       <GenreList/>
+      <FilmList/>
     </div>
   );
 };
