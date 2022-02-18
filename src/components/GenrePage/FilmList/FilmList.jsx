8045fc764216598d09d6c44c84240418996bcdf2
Androseiv <kakatinushka@gmail.com>
2022-02-18 18:15:29 +0200

    reload bug fix

--- src/components/GenrePage/FilmList/FilmList.jsx
+++ src/components/GenrePage/FilmList/FilmList.jsx
@@ -20,7 +20,7 @@ const FilmList = () => {
          setCurrentPage(1)
          setRequestStatus(true)
          setFilmList([])
-    },[selectedGenres, params.type, params.sortMethod])
+    },[selectedGenres, params])
 
     useEffect(() => {
         const sortMethod = params.sortMethod + '.desc'
