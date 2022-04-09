d616b405e3c55f09dcd211501af62beca91bd938
Androseiv <kakatinushka@gmail.com>
2022-04-09 18:54:01 +0300

    bug with genre unselect fixed

--- src/components/GenrePage/GenreList/GenreColumn/GenreItem/GenreItem.jsx
+++ src/components/GenrePage/GenreList/GenreColumn/GenreItem/GenreItem.jsx
@@ -33,28 +33,29 @@ const GenreItem = ({genre, selectedGenres, setSelectedGenres, clear}) => {
     }
   }
 
+
   return (
     <div className="genre-list__item">
       <label className='genre-list__label'>
-        <input id={genre.id} type="radio" className='genre-list__yes' name={genre.id} onChange={null} defaultChecked={yes} onClick={() => {
+        <input id={genre.id} type="radio" className='genre-list__yes' name={genre.id} onChange={() => {}} checked={yes} onClick={() => {
           if(yes){
             unselectToggle(genre.id)
           }else{
             changeGenres(!yes)
             setNo(false)
           }
-          setYes(!yes)
+          setYes(prev => !prev)
         }}/>
       </label>
       <label className='genre-list__label'>
-        <input type="radio" id={genre.id + 1} className='genre-list__no' name={genre.id} onChange={null} defaultChecked={no} onClick={() => {
+        <input type="radio" id={genre.id + 1} className='genre-list__no' name={genre.id} onChange={() => {}} checked={no} onClick={() => {
           if(no){
             unselectToggle(genre.id)
           }else{
             changeGenres(no)
             setYes(false)
           }
-          setNo(!no)
+          setNo(prev => !prev)
         }}/>
       </label>
       <label htmlFor={genre.id} className="genre-list__name">
